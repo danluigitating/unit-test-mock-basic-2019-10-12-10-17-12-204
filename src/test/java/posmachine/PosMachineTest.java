132ec32316b6a1e6dcdfdb4622782dd07bde2c62
DAN LUIGI TATING (SHP-IRIS-ISD-OOCLL/MNL) <TATINDA@OOCL.COM>
2019-10-14 11:47:25 +0800

    Add test cases

--- src/test/java/posmachine/PosMachineTest.java
+++ src/test/java/posmachine/PosMachineTest.java
@@ -1,7 +1,11 @@
 package posmachine;
 
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import static org.mockito.Mockito.when;
 
 public class PosMachineTest {
 
@@ -9,22 +13,41 @@ public class PosMachineTest {
     @Test
     public void should_get_receipt_using_real_price_calculator() {
         //given
+        PriceCalculator calculator = new PriceCalculator();
+        PosMachine posMachine = new PosMachine(calculator);
         //when
+
         //then
+        Assertions.assertThrows(UnsupportedOperationException.class, () -> posMachine.getReceipt("Coke"));
     }
 
     @Test
     public void should_get_receipt_using_sub_price_calculator() {
         //given
+        PriceCalculator calculator= new StubPriceCalculator();
+        PosMachine posMachine= new PosMachine(calculator);
+
         //when
         //then
+        Assertions.assertEquals(posMachine.getReceipt("Coke"), "Name: Coke, Price: 10.0");
     }
 
     @Test
     public void should_get_receipt_using_real_price_calculator_with_mockito() {
         //given
+        PriceCalculator calculator= Mockito.mock(PriceCalculator.class);
+        PosMachine posMachine= new PosMachine(calculator);
+
         //when
+        when(calculator.calculate("Coke")).thenReturn(10.0);
         //then
+        Assertions.assertEquals(posMachine.getReceipt("Coke"), "Name: Coke, Price: 10.0");
     }
 
+    private class StubPriceCalculator extends PriceCalculator {
+        @Override
+        public double calculate(String productName) {
+            return 10.0;
+        }
+    }
 }
